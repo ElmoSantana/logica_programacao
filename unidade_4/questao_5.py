1070c9892280837d857617c313a874df431a6424
ElmoSantana <antonielmoslima@gmail.com>
2025-07-14 00:30:01 -0300

    Update questao_5.py
    Corrigindo a questão 5.

--- unidade_4/questao_5.py
+++ unidade_4/questao_5.py
@@ -4,20 +4,18 @@ Deve conter pelo menos uma letra maiúscula.
 Deve conter pelo menos um número.
 Não pode conter espaços em branco."""
 
-senha = input("Digite sua senha com oito caracteres : ")
+while True:
+    
+    senha = str(input("Digite os caracteres para criar sua senha: "))
 
-quantid_caracteres = len(senha)
+    if len(senha) <7:
+        print("A senha deve conter pelo menos 8 (oito) caracteres: ")
+        if senha.islower():
+            print("A senha deve conter pelo menos um dos caracteres MAIÚSCULO: ")
+            if senha.isalnum():
+                print("A senha deve conter pelo menos um número: ")
+                if senha.isalpha ():
+                    print("A senha deve conter pelo menos um caractere especial: ")
 
-letras = set(("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"))
-numeros = set((1, 2, 3, 4, 5, 6, 7, 8, 9, 0))
-caracteres_especial = set(("!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\", "^", "{", "}"))
-
-if quantid_caracteres < 8:
-    print("Digite uma senha com no mínimo oito caracteres.")
-elif senha == "1, 2, 3, 4, 5, 6, 7, 8, 9, 0":
-        if senha == "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z":
-            if senha == "!, #, $, %, &, ', (, ), *, +, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, ~":
-                 if quantid_caracteres >= 8:
-                      print("Senha cadastrada com sucesso.")
-else:
-    print("Senha inválida.")
+print("Senha cadastrada com sucesso!")
+    
