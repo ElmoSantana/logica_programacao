9a6e14728ffb96a7df990b9ce32c8039b4747d50
ElmoSantana <antonielmoslima@gmail.com>
2025-08-08 23:49:31 -0300

    Unidade 5
    Inicio da Unidade 5

---
+++ unidade_5/aula.py
@@ -0,0 +1,7 @@
+for c in range(1, 11):
+    velocidade = float(input("Informe a velocidade em km: "))
+
+if c /10 > 60:
+    print("Velocidade média acima do limite estabelecido.")
+else:
+    print("Velocidade média está no limite estabelecido.")
