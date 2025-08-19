198b742abf7b5716264cf1627b97cd37f7bd1a69
ElmoSantana <antonielmoslima@gmail.com>
2025-08-19 06:26:48 -0300

    Unidade 5

--- unidade_5/aula.py
+++ unidade_5/aula.py
@@ -1,5 +1,21 @@
-frase = str(input("Digite uma frase: "))
-for vogais in frase:
-    if vogais.lower() in 'aãáàeéiíoõóuú':
-        tamanho = len(vogais)
-        print('O número de vogais nessa frase é de: ', tamanho)
+"""Programa que lê uma matriz 3x3 digitada pelo usuário e conta quantos números pares existem na matriz,
+imprimindo na tela o resultado e a matiz."""
+
+matriz = []
+for i in range(3):
+    linha = []
+    for j in range(3):
+        msg = f"Número da célula [{i}] [{j}]?"
+        linha.append(int(input(msg)))
+    matriz.append(linha)
+
+pares = 0
+for linha in matriz:
+    for e in linha:
+        if e % 2 == 0:
+            pares += 1
+
+for linha in matriz:
+    print(linha)
+
+print(f"A matriz contém {pares} numeros pares.")
